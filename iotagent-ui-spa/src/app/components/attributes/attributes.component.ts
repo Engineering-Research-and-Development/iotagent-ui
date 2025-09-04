b35a5c64afe6904ca1366cc334dc503b2a5a5b35
ludilorenz <Luca.DiLorenzo@eng.it>
2025-09-04 10:29:02 +0200

    New layout for attributes modal

--- iotagent-ui-spa/src/app/components/attributes/attributes.component.ts
+++ iotagent-ui-spa/src/app/components/attributes/attributes.component.ts
@@ -9,9 +9,37 @@ import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 export class AttributesComponent {
   
   data: any;
+  showedData: any;
 
   constructor(private dialogConfig: DynamicDialogConfig) {
     this.data = this.dialogConfig.data.data;
+    this.showedData = this.data;
+  }
+
+  searchAttribute(attribute: any){
+    if(attribute.length > 0){
+      this.showedData = this.data.filter((attr: any) => attr.name.toLowerCase().includes(attribute.toLowerCase()));
+    }else{
+      this.refreshData();
+    }
+  }
+
+  interactiveSearchAttribute(attribute: any){
+    if(attribute.length > 2){
+      this.searchAttribute(attribute);
+    }else if(attribute.length === 0){
+      this.refreshData();
+    }
+  }
+
+  refreshData(){
+    let elem = document.getElementById("filter_attribute");
+    (elem as HTMLInputElement).value = '';
+    this.showedData = this.data;
+  }
+
+  refreshAttributeFilter(){
+    this.refreshData();
   }
 
 }
